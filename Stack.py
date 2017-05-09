5d718a423de08edd0b951338b8a05984b9049c85
Feiteng <fl750@nyu.edu>
2017-05-09 17:42:14 -0400

    .

---
+++ Stack.py
@@ -0,0 +1,11 @@
+def gameOfTwoStacks():
+    for _ in range(int(input())):
+        n, m, x = map(int, input().split(' '))
+        a = list(map(int, input().split(' ')))
+        b = list(map(int, input().split(' ')))
+
+
+
+
+
+gameOfTwoStacks()
